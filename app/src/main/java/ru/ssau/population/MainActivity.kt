b97b49209fb69224729161b313beb144fe952b26
Fomin <vyacheslav.fomin@magenta-technology.com>
2022-12-17 10:21:48 +0400

    Initial commit

---
+++ app/src/main/java/ru/ssau/population/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.ssau.population
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.ssau.population.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/ssau/population/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.ssau.population.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
