a3c6d02d1bd640913b4894ffa86faebb8f44523b
Aragamin <107625196+Aragamin@users.noreply.github.com>
2022-12-18 15:53:33 +0400

    Mapping UI parameters

--- app/src/main/java/ru/ssau/population/ui/screen/PrepareFragment.kt
+++ app/src/main/java/ru/ssau/population/ui/screen/PrepareFragment.kt
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import ru.ssau.population.R
 import ru.ssau.population.databinding.FragmentPrepareBinding
+import ru.ssau.population.model.PopulationParameters
+import java.lang.Exception
 
 class PrepareFragment : Fragment() {
 
@@ -24,6 +27,29 @@ class PrepareFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.producer.alpha
+        binding.buttonNext.setOnClickListener {
+            val populations: List<PopulationParameters> = listOf(binding.producer, binding.predator, binding.apexPredator).map {
+                val (alpha, beta, t, omega, i) = listOf(
+                    it.alphaLayout to it.alpha,
+                    it.betaLayout to it.beta,
+                    it.tLayout to it.t,
+                    it.omegaLayout to it.omega,
+                    it.iLayout to it.i,
+                ).map { (layout, input) ->
+                    val value: Double
+                    try {
+                        value = input.text.toString().toDouble()
+                    } catch (exception: Exception) {
+                        layout.error = getString(R.string.error_value)
+                        return@setOnClickListener
+                    }
+                    value
+                }
+                PopulationParameters(alpha, beta, t, omega, i)
+            }
+
+        }
         // get/set parameters view
     }
 
